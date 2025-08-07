c5e04853de5a26d97679ba66db4248414b3713d3
sahilinamdar-lang <sahilinamdar212@gmail.com>
2025-08-07 13:52:02 +0530

    Connection to database

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -1,5 +1,17 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
+	private static final String URL = "jdbc:mysql://localhost:3306/BankDB";
+	private static final String USER = "root";
+	private static final String PASSWORD = "Sahil2410";
+
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+	
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
 }
