a20a8d2c1b066ead2933eb4c240f11596d9b9789
sahilinamdar-lang <sahilinamdar212@gmail.com>
2025-08-07 16:36:01 +0530

    user model add

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,6 +1,108 @@
 package model;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
 public class User {
-	
+
+	private int userId;
+	private String fullName;
+	private String email;
+	private String password;
+	private String phoneNumber;
+	private String address;
+	private Date dateOfBirth;
+	private String gender;
+	private Timestamp createdAt;
+	private Integer approvedBy; // Nullable
+	private String approvalStatus;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Integer getApprovedBy() {
+		return approvedBy;
+	}
+
+	public void setApprovedBy(Integer approvedBy) {
+		this.approvedBy = approvedBy;
+	}
+
+	public String getApprovalStatus() {
+		return approvalStatus;
+	}
+
+	public void setApprovalStatus(String approvalStatus) {
+		this.approvalStatus = approvalStatus;
+	}
 
 }
